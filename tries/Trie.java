a0ccb633a10efdb3f81e53ea5934a5356a855703
JamesCordova <jcordovas@unsa.edu.pe>
2023-07-21 08:45:53 -0500

    Trie con algunas implementaciones en insert y métodos declarados

---
+++ tries/Trie.java
@@ -0,0 +1,26 @@
+public class Trie {
+  public TrieNode root;
+
+  public Trie() {
+    root = new TrieNode();
+  }
+
+  public void insert(String word) {
+    TrieNode node = root;
+    for (char c : word.toCharArray()) {
+      if (node.children[c - 'a'] == null) {
+        node.children[c - 'a'] = new TrieNode();
+      }
+      node = node.children[c - 'a'];
+    }
+    node.isEndWord = true;
+  }
+
+  public boolean search(String word) {
+    return false;
+  }
+
+  public void replace(){
+
+  }
+}
